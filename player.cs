e99300d686f3063facde46b8651e7c453fd87160
Elyon74 <48656046+Elyon74@users.noreply.github.com>
2021-09-13 23:52:04 +0200

    1.0
    In Testing

--- player.cs
+++ player.cs
@@ -7,10 +7,12 @@ public class player : Sprite
     // private int a = 2;
     // private string b = "text";
 
+    public int HP = 3;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
